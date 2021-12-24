6dbd7c3becb01e3a67c0e880cabf5534a20f3ccb
post1blues <yura1kovalyov@gmail.com>
2021-12-24 02:46:27 +0200

    Fix misspelling

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -90,7 +90,7 @@ MEDIA_URL = '/media/'
 db_url = env('DB_URL')
 
 DATABASES = {
-    'default2': dj_database_url.parse(db_url, conn_max_age=600)
+    'default': dj_database_url.parse(db_url, conn_max_age=600)
 }
 
 AUTH_PASSWORD_VALIDATORS = [
